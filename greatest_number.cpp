e26c18511e7682e97071a3efbfa16b8e4522cdc2
Cameron diedrich <cameron.diedrich@mths.ca>
2022-11-09 15:42:35 +0000

    change

--- greatest_number.cpp
+++ greatest_number.cpp
@@ -5,22 +5,24 @@
 // This program finds the greatest of three numbers
 
 #include <iostream>
-using namespace std;
 
 int main() {
     float n1, n2, n3;
 
-    cout << "Enter three numbers: ";
-    cin >> n1 >> n2 >> n3;
+    // input
+    std::cout << "Enter first number: " << std::endl;
+    std::cout << "Enter second number: " << std::endl;
+    std::cout << "Enter third number: " << std::endl;
 
+    // process and output
     if (n1 >= n2 && n1 >= n3)
-        cout << "Largest number: " << n1;
+        std::cout << "Largest number: " << n1;
 
     if (n2 >= n1 && n2 >= n3)
-        cout << "Largest number: " << n2;
+        std::cout << "Largest number: " << n2;
 
     if (n3 >= n1 && n3 >= n2)
-        cout << "Largest number: " << n3;
+        std::cout << "Largest number: " << n3;
 
-    return 0;
+    std::cout << "\n\nDone.\n";
 }
